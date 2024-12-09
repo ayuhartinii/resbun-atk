095ef1cb0aae1f836868e9212ba889cb2aa50497
ayuhartinii <ayu.rectoverso@gmail.com>
2024-12-09 08:09:03 +0700

    Add files via upload

---
+++ Script0.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('menuToggle').addEventListener('click', () => {
+    const navLinks = document.getElementById('navLinks');
+    navLinks.classList.toggle('show');
+});
